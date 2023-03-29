3a1b1a8611281fb0b70f2e0083a6b0d4cc5f13b1
zoro-diablo <vyshnavk117@gmail.com>
2023-03-29 16:20:58 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '30318e80-bf93-4b76-9550-c846d9aa9af5';
+export const MODAL_CLOSE_SEC = 2.5;
